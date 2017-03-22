ecb892906039a0e398a5dfe4b9a1f8f436439c72
Damien Kerbart <damien.kerbart@mnt.fr>
2017-03-22 10:03:07 +0100

    fake test

--- src/test/java/com/kerbart/match/CryptoTest.java
+++ src/test/java/com/kerbart/match/CryptoTest.java
@@ -1,15 +1,6 @@
 package com.kerbart.match;
 
-import static org.junit.Assert.assertEquals;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.RandomStringUtils;
+import com.kerbart.match.spring.AppConfiguration;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
@@ -17,16 +8,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.kerbart.match.exceptions.CryptoException;
-import com.kerbart.match.helper.CryptoUtils;
-import com.kerbart.match.spring.AppConfiguration;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("/META-INF/application-context-test.xml")
 @SpringApplicationConfiguration(classes = AppConfiguration.class)
 @WebAppConfiguration
 public class CryptoTest {
-
-
+    @Test
+    public void fake() {
+        System.out.println("OK ! ahah");
+    }
 
 }
